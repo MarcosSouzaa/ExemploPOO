5419873852e80a03a62610b8e65f5bba072be136
MarcosSouzaa <marcossouza.git@gmail.com>
2024-11-27 22:00:32 -0300

    trabalhando com herança e Polimorfismo

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -9,10 +9,12 @@ namespace ExemploPOO.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; } 
+        public string Email { get; set; }
 
         public void Apresentar() 
         {
-            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos\n" +
+                             $"e meu email é {Email}.\n");
         } 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// Exportando o Models com a classe
 using System.Security.Authentication;
+using System.Security.Cryptography;
 using ExemploPOO.Models;
 
 //******* Criando um objeto p1 do tipo Pessoa estanciando aqui a classe aqui **********
@@ -12,10 +13,23 @@ using ExemploPOO.Models;
 
 //******* Criando o objeto ContaCorrente e estanciando a classe com o seu construtor
 
-ContaCorrente c1 = new ContaCorrente(010056, 1000);
+// ContaCorrente c1 = new ContaCorrente(010056, 1000);
 
-Console.WriteLine("Conta corrente Nº " + c1.NumeroConta + "\n");
+// Console.WriteLine("Conta corrente Nº " + c1.NumeroConta + "\n");
 
- c1.Deposito(2000);
- c1.Sacar(4200);
+//  c1.Deposito(2000);
+//  c1.Sacar(4200);
 
+Aluno a1 = new Aluno();
+
+a1.Nome = "João";
+a1.Idade = 20;
+a1.Email = "teste@teste.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor pr = new Professor();
+pr.Nome = "Gisele";
+pr.Idade = 50;
+pr.Email = "gisele@gmail.com";
+pr.Apresentar();
