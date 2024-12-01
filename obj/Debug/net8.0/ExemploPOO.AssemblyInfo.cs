fdd6e6df1477de2b88ea8cdccea73b6fd398d15f
MarcosSouzaa <marcossouza.git@gmail.com>
2024-12-01 00:05:48 -0300

    Classe mãe Object

--- obj/Debug/net8.0/ExemploPOO.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExemploPOO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4626b8339da459841db7f230e0cb7d0f43bc95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69090fc7839bf46eed3534499a41803c6a3468c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
